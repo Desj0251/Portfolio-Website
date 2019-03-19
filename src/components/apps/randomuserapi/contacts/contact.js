e803a19b534264b3b08d9e7f809ef347daa923ea
John Desjardins <desj0251@algonquinlive.com>
2019-03-18 20:24:26 -0400

    Contacts Example Complete!!

--- src/components/apps/randomuserapi/contacts/contact.js
+++ src/components/apps/randomuserapi/contacts/contact.js
@@ -78,7 +78,99 @@ function isActive(key, active) {
 // === Contact.Component.Details :
 // =================================================================================
 function UserDetails(props) {
-    return <div>{ props._activeUser.login.username }</div>
+    
+    const user = props._activeUser;
+    
+    return (
+        <div className="w-100 user-details">
+            <div class="media">
+                <img src={user.picture.large} class="mr-3 large-avatar" alt="..."/>
+                <div class="media-body">
+                    <h5 class="mt-0">{jsUcfirst(user.login.username)}</h5>
+                    <small class="muted my-0"><i>{user.email}</i></small>
+                    <div className="w-100 text-center mt-2">
+                        <DetailsTable 
+                            _user={user}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function DetailsTable(props) {
+
+    const user = props._user;
+
+    return (
+        <table class="table table-borderless table-sm table-responsive text-light">
+            <tbody>
+                <DetailsTableRow 
+                    _icon="fas fa-user" 
+                    _title="Name" 
+                    _content={jsUcfirst(user.name.first) + " " + jsUcfirst(user.name.last)}  
+                />
+                <DetailsTableRow 
+                    _icon="far fa-user" 
+                    _title="Gender" 
+                    _content={jsUcfirst(user.gender)}  
+                />
+                <DetailsTableRow 
+                    _icon="fas fa-calendar-day" 
+                    _title="Date of Birth" 
+                    _content={getDate(user.dob.date)}  
+                />
+                <DetailsTableRow 
+                    _icon="far fa-clock" 
+                    _title="Age" 
+                    _content={user.dob.age}  
+                />
+                <DetailsTableRow 
+                    _icon="fas fa-phone" 
+                    _title="Phone (Home)" 
+                    _content={user.phone}  
+                />
+                <DetailsTableRow 
+                    _icon="fas fa-mobile-alt" 
+                    _title="Phone (Cell)" 
+                    _content={user.cell}  
+                />
+                <DetailsTableRow 
+                    _icon="fas fa-home" 
+                    _title="Address" 
+                    _content={`${capital_letter(user.location.street)}.`}  
+                />
+                <DetailsTableRow 
+                    _icon={''} 
+                    _title={''} 
+                    _content={`${jsUcfirst(user.location.city)}, ${jsUcfirst(user.location.state)}`}  
+                />
+                <DetailsTableRow 
+                    _icon={''} 
+                    _title={''} 
+                    _content={`Canada, ${user.location.postcode}`}  
+                />
+            </tbody>
+        </table>
+    );
+}
+
+function getDate(date_string) {
+    var d = new Date(date_string);
+    var options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return d.toLocaleDateString("en-US", options);
+}
+
+function DetailsTableRow(props) {
+    return (
+        <tr>
+            <td className="align-middle text-center"><small><i class={props._icon}></i></small></td>
+            <td className="align-middle text-left"><small>{props._title}</small></td>
+            <td className="align-middle"><small>:</small></td>
+            <td className="align-middle text-left"><small className="muted">{props._content}</small></td>
+        </tr>
+    );
 }
 
 // =================================================================================
@@ -99,19 +191,15 @@ function Error(props) {
     );
 }
 
-function Spinner(props) {
-    return (
-        <div className="spinner-grow text-light mx-2" role="status" /> );
-}
 function ContactHomeLoading(props) {
     return (
         <div className="spinner row align-items-center w-100 m-0">
             <div className="col text-center">
-                <Spinner />
-                <Spinner />
-                <Spinner />
-                <Spinner />
-                <Spinner />
+                <div className="spinner-grow text-light mx-2" role="status" />
+                <div className="spinner-grow text-light mx-2" role="status" />
+                <div className="spinner-grow text-light mx-2" role="status" />
+                <div className="spinner-grow text-light mx-2" role="status" />
+                <div className="spinner-grow text-light mx-2" role="status" />
             </div>
         </div>
     );
@@ -134,3 +222,12 @@ let nestedSort = (prop1, prop2 = null, direction = 'asc') => (e1, e2) => {
 }
 
 function jsUcfirst(string) { return string.charAt(0).toUpperCase() + string.slice(1); }
+
+function capital_letter(str) 
+{
+    str = str.split(" ");
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+    return str.join(" ");
+}
